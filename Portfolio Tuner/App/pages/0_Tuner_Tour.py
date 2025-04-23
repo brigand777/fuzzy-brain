21b4e7e3250491a75490716eb5575b4b7b4c4557
brigand777 <kfernando300@yahoo.com>
2025-04-23 03:01:34 -0400

    Adding comments to tuner tour

--- Portfolio Tuner/App/pages/0_Tuner_Tour.py
+++ Portfolio Tuner/App/pages/0_Tuner_Tour.py
@@ -145,7 +145,11 @@ selected_assets = portfolio_df["Asset"].dropna().unique().tolist()
 
 # --- Step 2: Date Range ---
 #st.header("📆 Step 2: Select Your Date Range")
-section_heading("📆 Step 2: Select Your Date Range", short_description="Adjusting which time period you want to understand more about is going to be an ongoing thing...hindsight is great!", level=2)
+st.markdown(section_heading(
+    "📆 Step 2: Select Your Date Range",
+    short_description="Adjusting which time period you want to understand more about is going to be an ongoing thing...hindsight is great!",
+    level=2
+), unsafe_allow_html=True)
 
 narrative("Choose a time period to analyze your portfolio’s past performance. We’ll default to the last 90 days, but you can adjust the dates as needed.", color="#1F77B4")
 
